a285e51676f192d23981c9ef1ade8e0f5509764e
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-11 15:24:48 +0600

    function explored a little bit

--- first.ts
+++ first.ts
@@ -24,3 +24,19 @@ const myName:string="Emi";
 // but no restriction is for let cause we can assing value to it when we want
 let yourName:boolean;
 yourName=true;
+
+// function related simple discusion 
+function add (num1:number , num2:number):number // this will define the value type of number 
+{
+return num1+num2
+}
+// we can't even set the value of that variable by which we are calling the function it is already been set by the type of return
+const addCaller = add(2,4);
+ // if we set type of a function to void it will only return undefined 
+ const  doubleItAndConsole =() => void{
+
+}
+ // it will retunr automatically  it's type somethimes  and we can declare a function like this way and can set up value by another line like variable
+let multiply =(x:number , y :number) :number=>
+x*y
+multiply=(x,y)=>x*y // here we cannot pass strings as paramitters
